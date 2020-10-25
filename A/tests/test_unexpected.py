d5e2a5735465b20e9d72a7a319b822c2f5188469
Er Jun Li <er.li@mail.mcgill.ca>
2020-10-25 01:29:17 -0400

    completed unexpected tests

--- A/tests/test_unexpected.py
+++ A/tests/test_unexpected.py
@@ -4,7 +4,10 @@ import requests
 import xmltodict
 import json
 
-""" This file evaluates any unexpected side effects with all endpoints (testing endpoints with unexpected data) """
+""" This file evaluates any unexpected side effects with all endpoints by testing each endpoint with unexpected data. """
+
+# NOTE: if we delete last task in a category it deletes itself (BUG)
+# NOTE: if we delete last task in a project it deletes itself (BUG)
 
 #######################
 ###     /todos      ###
@@ -33,7 +36,7 @@ def test_post_todos_id():
     # NOTE: when sending empty data to POST /todos/:id, we don't get an error mesage and the request successful
     assert r.status_code == 200 and res == todo
 
-def test_double_delete_todos_id(jar):
+def test_double_delete_todos_id():
     deleted_todo = requests.get(url_id % 2, headers=recv_json_headers).json()["todos"][0]
     r = requests.delete(url_id % 2, headers=send_json_recv_json_headers)
 
@@ -47,32 +50,40 @@ def test_double_delete_todos_id(jar):
 ###   /todos/:id/tasksof    ###
 ###############################
 
-
 def test_post_todos_id_tasksof():
     """ Create an instance of a relationship named tasksof between todo instance :id and the project instance represented by the id in the body of the message """
     
     # first create a new todo
-    r = requests.post(url,data=todo_xml2, headers=send_xml_recv_xml_headers)
-    todo = json.loads(json.dumps(xmltodict.parse(r.text)))["todo"]
+    create = requests.post(url,data=todo_xml2, headers=send_xml_recv_xml_headers)
+    todo = json.loads(json.dumps(xmltodict.parse(create.text)))["todo"]
     todo_id = int(todo["id"])
 
     # create project
     project = """<project><id>1</id></project>"""
+    # NOTE: since we can't take ids in XML, this gives a BUG
+    # This returns {"errorMessages": ["Could not find thing matching value for id"]}    
     
     # create a relationship between todo instance we just created and the project instance represented by the id in the body of the message
     r = requests.post(url_id_tasksof % todo_id, data=project, headers=send_xml_recv_xml_headers)
     # NOTE: this gives us project id not found BUG not working, can't create relationships using XML, also it is not documented how the XML should be
 
     # verify that we created a tasksof relationship with "id" == 1
-    modified_todo = requests.get(url_id % todo_id , headers=recv_xml_headers).json()["todos"][0]
+    modified_todo = requests.get(url_id % todo_id , headers=recv_xml_headers).text
+    # assert r.status_code == 201 and "tasksof" in modified_todo and project in modified_todo
     
-    assert r.status_code == 201 and "tasksof" in modified_todo and project in modified_todo
+    # failed to create relationship
+    assert r.status_code == 404 and "tasksof" not in modified_todo and project not in modified_todo and "error" in r.text
 
 ###################################
 ###   /todos/:id/tasksof/:id    ###
 ###################################
 
-# forget to put an id: /todos/tasksof test no ide before tasksof and no id after tasksof
+def test_get_todos_tasksof():
+    # NOTE: This is an undocumented behaviour where /todos/tasksof is redirecting us to /projects (BUG, shouldn't accept this path without :ids)
+    r = requests.get("http://localhost:4567/todos/tasksof", headers=recv_json_headers)
+    projects = r.json()
+    # make sure only one id object is returned
+    assert r.status_code == 200 
 
 ##################################
 ###   /todos/:id/categories    ###
@@ -82,15 +93,12 @@ def test_post_todos_id_categories():
     """ Create an instance of a relationship named categories between todo instance :id and the category instance represented by the id in the body of the message  """
     
     # first create a new todo
-    r = requests.post(url,data=todo_xml3, headers=send_xml_recv_xml_headers)
-    todo = json.loads(json.dumps(xmltodict.parse(r.text)))["todo"]
+    create = requests.post(url,data=todo_xml3, headers=send_xml_recv_xml_headers)
+    todo = json.loads(json.dumps(xmltodict.parse(create.text)))["todo"]
     todo_id = int(todo["id"])
 
     # create project
-    category ="""<category>
-    <id></id>
-    </category>
-    """
+    category = """<category><id>1</id></category>"""
     
     # create a relationship between todo instance we just created and the project instance represented by the id in the body of the message
     r = requests.post(url_id_categories % todo_id, data=category, headers=send_xml_recv_xml_headers)
@@ -98,11 +106,20 @@ def test_post_todos_id_categories():
     # verify that we created a categories relationship with "id" == 1
     modified_todo = requests.get(url_id % todo_id , headers=recv_xml_headers).text
     
-    assert r.status_code == 201 and "categories" in modified_todo and category in modified_todo
+    # assert r.status_code == 201 and "categories" in modified_todo and category in modified_todo
+    assert r.status_code == 404 and "categories" not in modified_todo and category not in modified_todo and "error" in r.text
+
 
 
 ######################################
 ###   /todos/:id/categories/:id    ###
 ######################################
 
-# forget to put an id: /todos/categories test no ide before category and no id after category
+
+def test_get_todos_categories():
+    # NOTE: This is an undocumented behaviour where /todos/categories is redirecting us to /categories (BUG, shouldn't accept this path without :ids)
+    r = requests.get("http://localhost:4567/todos/categories", headers=recv_json_headers)
+    categories = r.json()
+    # make sure only one id object is returned
+    assert r.status_code == 200 
+
