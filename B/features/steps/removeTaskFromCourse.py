cae0574e307cea2693908015b8a85d92b8ef2114
ding-ma <ding.ma@mail.mcgill.ca>
2020-11-16 15:32:08 -0500

    fixed assertions

--- B/features/steps/removeTaskFromCourse.py
+++ B/features/steps/removeTaskFromCourse.py
@@ -4,35 +4,6 @@ from behave import *
 from features.steps.helper import *
 
 
-@when(u'a user removes the following task from the course to do list')
-def step_impl(context):
-    todos = requests.get(url_todo).json()["todos"]
-    for row in context.table:
-
-        for todo in todos:
-            if row["task_title"] == todo["title"] and row["task_description"] == todo["description"] and row[
-                "task_doneStatus"] == todo["doneStatus"]:
-                # delete this todo
-                deleted_todo = requests.get(url_todo_id % int(todo["id"]), headers=recv_json_headers).json()["todos"][0]
-                r = requests.delete(url_todo_id % int(todo["id"]), headers=send_json_recv_json_headers)
-                new_todos = requests.get(url_todo).json()["todos"]
-
-                assert r.status_code == 200 and deleted_todo not in new_todos
-
-
-@when(u'a user removes a task that does not exist to a course todo list')
-def step_impl(context):
-    todos = requests.get(url_todo).json()["todos"]
-    for row in context.table:
-        for todo in todos:
-            if row["task_title"] == todo["title"] and row["task_description"] == todo["description"] and row[
-                "task_doneStatus"] == todo["doneStatus"]:
-                # delete this todo
-                deleted_todo = requests.get(url_todo_id % int(todo["id"]), headers=recv_json_headers).json()["todos"][0]
-                r = requests.delete(url_todo_id % int(todo["id"]), headers=send_json_recv_json_headers)
-                assert r.status_code == 404 and "errorMessages" in r.json()
-
-
 @when("a user removes that task from the course to do list")
 def step_impl(context):
     """
@@ -44,7 +15,13 @@ def step_impl(context):
     r = requests.delete(url_todo_id % int(context.task['id']), headers=send_json_recv_json_headers)
     new_todos = requests.get(url_todo).json()["todos"]
     context.new_todos = new_todos
-    assert r.status_code == 200 and deleted_todo not in new_todos
+    for todo in new_todos:
+        assert r.status_code == 200 and not (
+                todo['title'] == deleted_todo['title'] and
+                todo['completed'] == deleted_todo['completed'] and
+                todo['active'] == deleted_todo['active'] and
+                todo['description'] == deleted_todo['description']
+        )
 
 
 @then("the task should not be contained")
@@ -52,7 +29,16 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    assert context.task not in context.projects
+    r = requests.get(url_project)
+    tasks = r.json()
+    not_exist_task = context.task
+    for task in tasks['projects']:
+        assert r.status_code == 200 and not (
+                task['title'] == not_exist_task['title'] and
+                task['completed'] == not_exist_task['completed'] and
+                task['active'] == not_exist_task['active'] and
+                task['description'] == not_exist_task['description']
+        )
 
 
 @when("a user removes that non existing task from the course to do list")
