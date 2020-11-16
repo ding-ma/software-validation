e5c1f487675b78147de79fde2a78cb2a835ca9e4
Ding Ma <43629633+ding-ma@users.noreply.github.com>
2020-11-16 11:57:44 -0500

    Todos list (#6)
    * fixed typos
    
    * fixed error case with createToDoList.feature with id
    
    * uncompleted task change

--- B/features/steps/createToDoList.py
+++ B/features/steps/createToDoList.py
@@ -28,7 +28,7 @@ def step_impl(context, project_title, project_description, project_completed, pr
 
 
 @then(
-    "the projects should a project with title {project_title}, description {project_description}, complete status {project_completed} and active status {project_active}")
+    "the projects should contain a project with title {project_title}, description {project_description}, complete status {project_completed} and active status {project_active}")
 def step_impl(context, project_title, project_description, project_completed, project_active):
     """
     :type context: behave.runner.Context
@@ -55,28 +55,6 @@ def step_impl(context, project_title, project_description, project_completed, pr
     assert projects.status_code == 200 and context.project_res not in projects.json()['projects']
 
 
-@when(
-    "a user creates a project without a title, description {project_description}, complete status {project_completed} and active status {project_active}")
-def step_impl(context, project_description, project_completed, project_active):
-    """
-    :type context: behave.runner.Context
-    :type project_description: str
-    :type project_completed: str
-    :type project_active: str
-    """
-    create_project = requests.post(url_project, data=json.dumps(
-        {
-            "id": 1,
-            "completed": bool(project_active),
-            "active": bool(project_completed),
-            "description": project_description
-        }), headers=send_json_recv_json_headers)
-    project_res = create_project.json()
-    projects = requests.get(url_project).json()["projects"]
-    context.project_res = project_res
-    assert create_project.status_code == 400 and 'errorMessages' in project_res
-
-
 @given(
     "there is not a project with title {project_title}, description {project_description}, complete status {project_completed} and active status {project_active}")
 def step_impl(context, project_title, project_description, project_completed, project_active):
@@ -90,3 +68,20 @@ def step_impl(context, project_title, project_description, project_completed, pr
     projects = requests.get(url_project)
     assert projects.status_code == 200 and project_title not in projects
     context.projects = projects
+
+
+@when("a user creates a project with id{id}")
+def step_impl(context, id):
+    """
+    :type context: behave.runner.Context
+    :type id: str
+    """
+    create_project = requests.post(url_project, data=json.dumps(
+        {
+            "id": id,
+
+        }), headers=send_json_recv_json_headers)
+    project_res = create_project.json()
+    projects = requests.get(url_project).json()["projects"]
+    context.project_res = project_res
+    assert create_project.status_code == 400 and 'errorMessages' in project_res
